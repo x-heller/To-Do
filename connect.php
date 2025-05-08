7122ecde67c0a5153c21c95a402bdcb6509de7fa
x-heller <xheller@stuba.sk>
2025-05-08 15:28:57 +0200

    Final

--- connect.php
+++ connect.php
@@ -3,7 +3,6 @@ require 'vendor/autoload.php';
 
 function getMongoDBConnection($collectionName) {
     $uri = 'mongodb+srv://Felhasználónév:Jelszó@cluster0.afhbz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-
     try {
         $client = new MongoDB\Client($uri);
         $db = $client->todo;
